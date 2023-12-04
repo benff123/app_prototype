a57274997cb2d704c9318b3a49e38dba3021f391
benff123 <ben.finlay.freundlich@googlemail.com>
2023-12-04 13:30:51 +0100

    commit

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "appprototype": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "appprototype351fd26d": {
       "AppClientID": "string",
